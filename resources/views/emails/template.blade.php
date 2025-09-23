dbfe500422eafb872decaa28a47f7c174f72f2cf
Maxime <maxime.betrisey@hotmail.com>
2025-09-23 15:39:32 +0200

    Correction email

--- resources/views/emails/template.blade.php
+++ resources/views/emails/template.blade.php
@@ -56,7 +56,7 @@
 
 </head><body style="font-family: Helvetica, sans-serif; -webkit-font-smoothing: antialiased; font-size: 16px; line-height: 1.3; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; background-color: #f4f5f6; margin: 0; padding: 0;">
 	<table role="presentation" border="0" cellpadding="0" cellspacing="0" class="body" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #f4f5f6; width: 100%;" width="100%" bgcolor="#f4f5f6">
-		<tr>
+		<!-- <tr>
 			<td colspan="3" style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top;" valign="top">
 				<table role="presentation" border="0" cellpadding="0" cellspacing="0" class="header" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #0f172a; width: 100%; padding: 24px;" width="100%" bgcolor="#0f172a">
 					<tr>
@@ -70,7 +70,7 @@
 
 				</table>
 			</td>
-		</tr>
+		</tr> -->
 		<tr>
 			<td style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top;" valign="top">&nbsp;</td>
 			<td class="container" style="font-family: Helvetica, sans-serif; font-size: 16px; vertical-align: top; max-width: <?= $mailData->bodyWidth ?>; padding: 0; padding-top: 24px; width: 100%; margin: 0 auto;" width="100%" valign="top">
